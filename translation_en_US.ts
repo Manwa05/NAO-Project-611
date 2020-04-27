672aa8996711b476531aaf264fd856d40746acf0
Mansi Wagh <58379454+Manwa05@users.noreply.github.com>
2020-04-27 00:11:08 -0400

    Add files via upload

---
+++ translation_en_US.ts
@@ -0,0 +1,180 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Namaskar!..Chala game khelu..khelnar ka?</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Namaskar!..Chala game khelu..khelnar ka?</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Hello! Would you like to play a game?</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Hello! Would you like to play a game?</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (1)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Pahila prashna..teen ani nau kiti?</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Pahila prashna..teen ani nau kiti?</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Let us start! How much is 5 plus 3?</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Let us start! How much is 5 plus 3?</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (2)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Harkat nahi..</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Harkat nahi..</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>No worries...</source>
+            <comment>Text</comment>
+            <translation type="unfinished">No worries...</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (3)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Shabaash!..pudhcha prashna..Mi murkha ahe ka?</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Shabaash!..pudhcha prashna..Mi murkha ahe ka?</translation>
+        </message>
+        <message>
+            <source>Awesome! that is the correct answer..next question is..Which is the going month?</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Awesome! that is the correct answer..next question is..Which is the going month?</translation>
+        </message>
+        <message>
+            <source>Awesome! that is the correct answer..next question is..Which is the ongoing month?</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Awesome! that is the correct answer..next question is..Which is the ongoing month?</translation>
+        </message>
+        <message>
+            <source>Awesome! that is the correct answer..Current score 1..next question is..Which is the ongoing month?</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Awesome! that is the correct answer..Current score 1..next question is..Which is the ongoing month?</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Awesome! that is the correct answer..your score is 1..next question is..Which is the ongoing month?</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Awesome! that is the correct answer..your score is 1..next question is..Which is the ongoing month?</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (4)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Chukla uttar..</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Chukla uttar..</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>That is incorrect..would you like to try again?</source>
+            <comment>Text</comment>
+            <translation type="unfinished">That is incorrect..would you like to try again?</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (5)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Great! that is the right answer..</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Great! that is the right answer..</translation>
+        </message>
+        <message>
+            <source>Great! that is the right answer..your score is 2</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Great! that is the right answer..your score is 2</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Great! that is the right answer..your score is 2.Next question..Do you wash your hands regularly?</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Great! that is the right answer..your score is 2.Next question..Do you wash your hands regularly?</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (6)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Whoops, it is the wrong answer..would you like to try again?</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Whoops, it is the wrong answer..would you like to try again?</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (7)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Very Good! Keep it up! It is a good habit indeed! Your score is now 3.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Very Good! Keep it up! It is a good habit indeed! Your score is now 3.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (8)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>That is  not good..You should wash your hands regularly.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">That is  not good..You should wash your hands regularly.</translation>
+        </message>
+    </context>
+</TS>
